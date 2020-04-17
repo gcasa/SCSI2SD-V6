52b41a7080f122f6f6cf42e3dd80c032bfb01ffc
Gregory Casamento <greg.casamento@gmail.com>
2020-04-17 12:54:11 -0400

    Fix libusb issues

--- src/scsi2sd-util6/Dfu.hh
+++ src/scsi2sd-util6/Dfu.hh
@@ -15,7 +15,7 @@
 //	You should have received a copy of the GNU General Public License
 //	along with SCSI2SD.  If not, see <http://www.gnu.org/licenses/>.
 
-#include <libusb.h>
+#include "libusb.h"
 
 #include <string>
 
